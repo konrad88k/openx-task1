d1965700e8ffcb7fc5dd2aa6964de0e4f690069d
konrad88k <konrad.0808.k@gmail.com>
2023-04-20 14:26:40 +0200

    Fix insert method. Add leafSearch method.

--- index.js
+++ index.js
@@ -25,8 +25,10 @@ class Tree {
             } else if (!temp.left && temp.left !== 0) {
                 temp.left = newNode;
                 return this;
-            } else {
+            } else if (this.height(temp.right) - this.height(temp.left) < 2) {
                 temp = temp.right;
+            } else {
+                temp = temp.left;
             }
         }
     }
@@ -39,7 +41,14 @@ class Tree {
 
     // 1. Search nodes without children.
     leafSearch() {
-        //return 'number'
+        let leafs = [];
+        function traverse(node) {
+            if (node.left || node.left === 0) traverse(node.left);
+            if (!node.left && node.left !== 0 && !node.right && node.right !== 0) leafs.push(node.value);
+            if (node.right || node.right === 0) traverse(node.right);
+        }
+        traverse(this.root);
+        return leafs;
     }
 
     // 2. Find longest path.
@@ -48,16 +57,13 @@ class Tree {
             let output = [];
             return output;
         }
-
         let right = this.longestPath(node.right);
         let left = this.longestPath(node.left);
-
         if (right.length < left.length) {
             left.unshift(node.value)
         } else {
             right.unshift(node.value)
         }
-
         return (left.length > right.length ? left : right);
     }
 
@@ -84,9 +90,11 @@ class Tree {
 }
 
 const myTree = new Tree;
-let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+// let numArray = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+let numArray = [5, 7, 3, 0, 1, 8, 5, 2, 5, 2];
 myTree.insertArray(numArray);
 
 console.dir(myTree); // print myTree
 console.log("Longest path:", myTree.pathPrint(myTree.root)); // print longest path with arrows (edges)
 console.log("Height of tree:", myTree.height(myTree.root)); // height of tree
+console.log("Leafs:", myTree.leafSearch()); // search all leafs
